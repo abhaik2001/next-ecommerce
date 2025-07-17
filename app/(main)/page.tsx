fb6ca5e9757c50a884fda6d25f20bdcd48d896c5
abhai k <158554525+abhaik2001@users.noreply.github.com>
2025-07-17 20:03:31 +0530

    install nextjs and tailwind css

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function homepage () {
   return (
-    <h1 className="text-4xl font-bold text-blue-600">Homepage</h1>
+    <h1 className="text-4xl font-bold text-blue-600">Homepagess</h1>
   )
 }
